a0c2e3e8240c293e2b4ac965d4c9995a16a13feb
Makireddy Bala Vighnesh Reddy <121003103+Makireddyvighnesh@users.noreply.github.com>
2026-05-09 00:01:17 +0530

    Update solution for transformers-tokenization

--- transformer/transformers-tokenization/transformers-tokenization.py
+++ transformer/transformers-tokenization/transformers-tokenization.py
@@ -1,67 +1,76 @@
+import numpy as np
 from typing import List, Dict
 
 class SimpleTokenizer:
     """
     A word-level tokenizer with special tokens.
     """
-    
+
     def __init__(self):
         self.word_to_id: Dict[str, int] = {}
         self.id_to_word: Dict[int, str] = {}
+        self.vocab_size = 0
 
-        # Fixed special tokens
-        self.word_to_id = {
-            "<PAD>": 0,
-            "<UNK>": 1,
-            "<BOS>": 2,
-            "<EOS>": 3
-        }
-
-        self.id_to_word = {v: k for k, v in self.word_to_id.items()}
-        self.vocab_size = 4
-    
+        # Special tokens
+        self.pad_token = "<PAD>"
+        self.unk_token = "<UNK>"
+        self.bos_token = "<BOS>"
+        self.eos_token = "<EOS>"
 
     def build_vocab(self, texts: List[str]) -> None:
         """
-        Build vocabulary from training texts.
+        Build vocabulary from a list of texts.
+        Add special tokens first, then unique words.
         """
 
-        words = set()
+        # Special tokens
+        self.word_to_id = {
+            self.pad_token: 0,
+            self.unk_token: 1,
+            self.bos_token: 2,
+            self.eos_token: 3
+        }
+
+        # Collect unique words
+        unique_words = set()
 
         for text in texts:
-            for word in text.split():
-                words.add(word)
+            words = text.lower().split()
+            unique_words.update(words)
 
-        for word in words:
-            if word not in self.word_to_id:
-                idx = self.vocab_size
-                self.word_to_id[word] = idx
-                self.id_to_word[idx] = word
-                self.vocab_size += 1
+        # Add sorted words
+        for idx, word in enumerate(sorted(unique_words), start=4):
+            self.word_to_id[word] = idx
 
+        # Reverse mapping
+        self.id_to_word = {
+            idx: word for word, idx in self.word_to_id.items()
+        }
+
+        # Vocabulary size
+        self.vocab_size = len(self.word_to_id)
 
     def encode(self, text: str) -> List[int]:
         """
         Convert text to list of token IDs.
-        Unknown words -> <UNK>
+        Use UNK for unknown words.
         """
 
-        ids = []
+        words = text.lower().split()
 
-        for word in text.split():
-            ids.append(self.word_to_id.get(word, self.word_to_id["<UNK>"]))
-
-        return ids
-
+        return [
+            self.word_to_id.get(word, self.word_to_id[self.unk_token])
+            for word in words
+        ]
 
     def decode(self, ids: List[int]) -> str:
         """
-        Convert token IDs back to text.
+        Convert list of token IDs back to text.
         """
 
-        words = []
-
-        for i in ids:
-            words.append(self.id_to_word.get(i, "<UNK>"))
+        words = [
+            self.id_to_word.get(i, self.unk_token)
+            for i in ids
+        ]
 
         return " ".join(words)
